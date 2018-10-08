312ccc08a62d4155a7d15bacb351df5482ffe862
ilyaYarosh0 <43451815+ilyaYarosh0@users.noreply.github.com>
2018-10-08 09:05:35 +0300

    Add files via upload

--- lab3/exer1/main.c
+++ lab3/exer1/main.c
@@ -4,6 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-
+	int a=130,b=160,i,sum=0,j;
+		for(i=0;i<9;i++){
+			sum+= b;		
+		}
+			for(j=0;j<12;j++){
+			sum+= a;
+		}
+	printf ("%d",sum);
 	return 0;
 }
