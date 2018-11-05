eb6b1a51f6ce06e78da1c095c2ed1fe12f1cebbf
ilyaYarosh0 <43451815+ilyaYarosh0@users.noreply.github.com>
2018-11-05 09:10:43 +0300

    Add files via upload

---
+++ lab7/exer2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <windows.h>
+void zamena(char stv[100], int ln1){
+	int j =0; int i; char res[100];
+for (i=0; i<ln1; i++){
+	if (stv[i]==' '){
+		if(j==0) continue;
+		if (stv[i+1]==' ') continue;
+		
+	}
+	res[j]=stv[i];
+	j++;
+}
+
+i=strlen(res);
+
+if(res[i-2]==' ')
+
+res[i-2]='\0';
+printf("\n%s",res);
+getchar();
+}
+
+int main(){ 
+setlocale(LC_ALL,"Russian"); 
+char str1[100];
+printf("¬ведите строку:");
+gets(str1);
+int lon1=srelen(str1);
+zamena(str1,lon1);
+}
+
